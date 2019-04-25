dc08ae65838a54da3544b09d9b03ea745e879ef1
luoxiaofei <thoughtworks.xie@daimler.com>
2019-04-25 09:49:48 +0800

    updata the demo

--- app/src/main/java/com/xiaofeiluo/viewtoimagedemo/MainActivity.java
+++ app/src/main/java/com/xiaofeiluo/viewtoimagedemo/MainActivity.java
@@ -51,6 +51,22 @@ public class MainActivity extends AppCompatActivity {
 
         return bmp;
     }
+
+    private Bitmap loadBitmapFromView2(View v) {
+        int w = v.getMeasuredWidth();
+        int h = v.getMeasuredHeight();
+
+        Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmp);
+
+        c.drawColor(Color.WHITE);
+        /** 如果不设置canvas画布为白色，则生成透明 */
+
+        v.layout(0, 0, w, h);
+        v.draw(c);
+
+        return bmp;
+    }
 }
 
 
---
+++ viewtoimage/src/main/java/com/xiaofeiluo/viewtoimage/ScreenUtil.java
@@ -0,0 +1,21 @@
+package com.xiaofeiluo.viewtoimage;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+public class ScreenUtil {
+
+
+    public static int getScreenWidth(Activity context) {
+        DisplayMetrics metric = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        return metric.widthPixels;     // 屏幕宽度（像素）
+    }
+
+    public static int getScreenHeight(Activity context) {
+        DisplayMetrics metric = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        return metric.heightPixels;   // 屏幕高度（像素）
+    }
+}
