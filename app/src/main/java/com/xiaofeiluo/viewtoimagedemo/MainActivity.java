84bfffefdec242d5987b7d7d9fbcd63397773703
luoxiaofei <thoughtworks.xie@daimler.com>
2019-04-26 16:35:05 +0800

    updata the demo

--- app/src/main/java/com/xiaofeiluo/viewtoimagedemo/MainActivity.java
+++ app/src/main/java/com/xiaofeiluo/viewtoimagedemo/MainActivity.java
@@ -13,14 +13,7 @@ import com.xiaofeiluo.viewtoimage.ScreenshotsHelper;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView image;
-    private ImageView image2;
-    private ImageView image3;
-    private ImageView image4;
-    private ImageView image5;
-    private ImageView image6;
-    private ImageView image7;
-    private ImageView image8;
-    private ImageView image9;
+
     private Button generate;
 
     @Override
@@ -30,14 +23,6 @@ public class MainActivity extends AppCompatActivity {
         final View goods_layout = View.inflate(this, R.layout.goods_layout, null);
         setContentView(view);
         image = findViewById(R.id.image);
-        image = findViewById(R.id.image2);
-        image = findViewById(R.id.image3);
-        image = findViewById(R.id.image4);
-        image = findViewById(R.id.image5);
-        image = findViewById(R.id.image6);
-        image = findViewById(R.id.image7);
-        image = findViewById(R.id.image8);
-        image = findViewById(R.id.image9);
         generate = findViewById(R.id.generate);
 
         generate.setOnClickListener(new View.OnClickListener() {
@@ -47,54 +32,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void getBitMap(Bitmap bitmap) {
                         image.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image2.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image3.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image4.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image5.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image6.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image7.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image8.setImageBitmap(bitmap);
-                    }
-                });
-                new ScreenshotsHelper().generateImages(MainActivity.this, goods_layout, new IBitMapCallBack() {
-                    @Override
-                    public void getBitMap(Bitmap bitmap) {
-                        image9.setImageBitmap(bitmap);
                     }
                 });
             }
